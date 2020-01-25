63d41ad06588c5ab180a406fe11b683cf766d612
Nasr Eldin <nasr2ldin@gmail.com>
2020-01-25 12:59:17 +0400

    Added Shared Kernel Class

---
+++ ChooseYourAdventure/ChooseYourAdventure.SharedKernel/Interfaces/IHandle.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ChooseYourAdventure.SharedKernel.Interfaces
+{
+    public interface IHandle<in T> where T : BaseDomainEvent
+    {
+        Task Handle(T domainEvent);
+    }
+}
